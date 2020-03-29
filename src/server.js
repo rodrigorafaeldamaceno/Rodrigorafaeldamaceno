f6f375df4eccc15cca42c2332d079cbc8e4a9cda
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2020-03-29 15:10:12 -0300

    heroku

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga')
 const path = require('path')
 const resolvers = require('./resolvers/resolvers')
+const port = process.env.PORT || 3000;
+
 
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, 'schema.graphql'),
   resolvers: resolvers
 })
 
-server.start({ port: 3000 })
-console.log('servidor online em http://localhost:3000/ ')
+server.start({ port })
+// console.log('servidor online em http://localhost:3000/ ')
